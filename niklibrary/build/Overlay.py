22d08ec0caf764225bb4afa5decca1bbf91464fc
Nikhil Menghani <nikhil@menghani.com>
2024-10-01 21:29:34 -0400

    Add the ability to extract overlays

--- niklibrary/build/Overlay.py
+++ niklibrary/build/Overlay.py
@@ -6,8 +6,10 @@ class Overlay:
     def __init__(self, overlay_path):
         self.path = overlay_path
 
-    def extract_overlay(self, extract_dir_path):
+    def extract_overlay(self, extract_dir_path=None):
         cmd = Cmd()
+        if extract_dir_path is None:
+            extract_dir_path = str(self.path).replace(".apk","") + "_extracted"
         if cmd.decompile_apk(self.path, extract_dir_path):
             return True
         return False
--- niklibrary/extract/ExtractOp.py
+++ niklibrary/extract/ExtractOp.py
@@ -1,3 +1,6 @@
+import os
+
+from niklibrary.build.Overlay import Overlay
 from niklibrary.helper.F import F
 from niklibrary.helper.Statics import Statics
 from niklibrary.oem.GoogleOem import GoogleOem
@@ -15,3 +18,14 @@ class ExtractOp:
                 c.extract_overlay(oem_overlays_dir)
             case _:
                 print("default")
+
+    @staticmethod
+    def extract_all_overlays(overlay_dir):
+        for root, _, files in os.walk(overlay_dir):
+            if root.lower().__contains__(os.sep + "overlay"):
+                for file in files:
+                    file_path = str(os.path.join(root, file))
+                    if file_path.lower().__contains__("overlay"):
+                        o = Overlay(file_path)
+                        if not o.extract_overlay():
+                            print("Overlay extraction of " + file_path + " failed")
