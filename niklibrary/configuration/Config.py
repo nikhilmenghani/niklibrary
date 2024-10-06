1de29d8d2e12f65614a578b90e60953adf741399
Nikhil Menghani <nikhil@menghani.com>
2024-10-06 17:18:56 -0400

    Add the ability to download a file

--- niklibrary/configuration/Config.py
+++ niklibrary/configuration/Config.py
@@ -6,3 +6,4 @@ load_dotenv()
 GITLAB_TOKEN = os.getenv("GITLAB_TOKEN")
 ENVIRONMENT_TYPE = os.environ.get('ENVIRONMENT_TYPE', 'dev')
 RELEASE_TYPE = os.environ.get('RELEASE_TYPE', 'stable')
+OEM = "caiman"
---
+++ test.py
@@ -0,0 +1,3 @@
+from niklibrary.helper.Assets import Assets
+
+print(Assets.get("apktool_2.10.0.jar"))
