eff966195a8461abf55f249705251981e9b37966
Nikhil Menghani <nikhil@menghani.com>
2025-03-24 18:01:06 -0400

    Test assets package with printing apktool path

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from nikassets.helper.Assets import Assets
+
 def main():
-    print("Hello World")
+    print("Hello World " + Assets.get("apktool_2.10.0.jar"))
 
 if __name__ == '__main__':
     main()
